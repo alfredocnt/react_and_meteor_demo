3b8bc7565f34581ceaea9bb80d48609b29a4ad28
alfredocnt <alfredo.cnt@gmail.com>
2017-03-11 14:03:24 -0300

    Added Bootstrap

--- client/components/image_list.js
+++ client/components/image_list.js
@@ -15,7 +15,7 @@ const ImageList = () => {
     return <ImageDetail image = {image} />
   });
   return (
-    <ul>
+    <ul className = "media-list list-group">
       {RenderedImages}
     </ul>
   );
